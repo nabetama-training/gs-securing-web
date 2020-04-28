ce24f11f9a449760511ed2ecd2c0dd10aae435c9
nabetama <mao.nabeta@gmail.com>
2020-04-28 23:27:43 +0900

    unsecure application

--- src/main/java/com/example/securingweb/MvcConfig.java
+++ src/main/java/com/example/securingweb/MvcConfig.java
@@ -2,9 +2,10 @@ package com.example.securingweb;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig {
+public class MvcConfig implements WebMvcConfigurer {
 
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.addViewController("/home").setViewName("home");
